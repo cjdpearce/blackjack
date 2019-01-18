81a84b32d3893ffae28f4742dbfbf235a87835bf
Craig Pearce <craig.pearce@skybettingandgaming.com>
2019-01-18 08:36:47 +0000

    began initial work on cardHand class

--- src/main/kotlin/com/cjdpearce/blackjack/LogicController.kt
+++ src/main/kotlin/com/cjdpearce/blackjack/LogicController.kt
@@ -1,15 +1,13 @@
 package com.cjdpearce.blackjack
 
 fun main(args: Array<String>) {
-    var playersHand: CardHand = CardHand()
+    var playersHand = CardHand()
 
     var cards:MutableMap<String,String> = mutableMapOf()
-    cards.put("123","abc")
-    cards.put("456","def")
-    cards.put("789","ghi")
+    cards.put("KH","10")
+    cards.put("7S","def")
     var cardKeys:Collection<String> = cards.keys
     var cardsShuffled:Collection<String> = cardKeys.shuffled()
 
-    playersHand.addCard(cards[cardsShuffled.first()]!!)
 }
 
--- src/main/kotlin/com/cjdpearce/blackjack/cardHand.kt
+++ src/main/kotlin/com/cjdpearce/blackjack/cardHand.kt
@@ -4,7 +4,18 @@ class CardHand {
     var cardHand:MutableMap<String,String> = mutableMapOf()
 
     fun addCard(card: String){
-        cardHand.put(card.toString(),"bdfjdfjfb")
-        println(cardHand)
+        cardHand.put(card,getCardValue(card))
+    }
+
+    fun returnTotal(){
+        var values = cardHand.values
+        var total = 0
+        for (value in values){
+            total += value.toInt()
+        }
+    }
+
+    private fun  getCardValue(card: String): String {
+        return card
     }
 }
