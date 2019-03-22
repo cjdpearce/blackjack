65c9e94b350fad8c7d5620a1ec08373e6cdb917b
Craig Pearce <craig.pearce@skybettingandgaming.com>
2019-03-22 16:54:54 +0000

    experimenting with how to draw blackjack app

--- src/main/kotlin/com/cjdpearce/blackjack/viewBuilder.kt
+++ src/main/kotlin/com/cjdpearce/blackjack/viewBuilder.kt
@@ -2,38 +2,111 @@ package com.cjdpearce.blackjack
 
 
 import javafx.geometry.Pos
-import javafx.scene.layout.BackgroundSize
 import javafx.scene.layout.StackPane
 import tornadofx.*
+import java.util.*
 
+class viewBuilder: View() {
+    // dependency injection
+    private val controller: viewController by inject()
 
-class viewBuilder : View("Blackjack") {
-
-    val numCol: Int = 8
-
-     //every view has a root component
+    // set up neighborhood
     override val root = gridpane {
-            add(background())
-            add(card(Pos.CENTER_LEFT))
-         add(card(Pos.CENTER))
+//        vgap = 15.0
+//        padding = insets(15)
+        row {
+            add(controller.background())
+            add(controller.background())
+            add(controller.background())
+            //add(controller.cardPane())
+            add(controller.background())
+            add(controller.background())
+        }
+        row{
+            add(controller.background())
+            add(controller.cardPane())
+            add(controller.background())
+            add(controller.cardPane())
+            add(controller.background())
+        }
+        row {
+            add(controller.background())
+            add(controller.background())
+            add(controller.background())
+            //add(controller.cardPane())
+            add(controller.background())
+            add(controller.background())
+        }
+        row {
+            add(controller.background())
+            add(controller.background())
+            add(controller.background())
+            //add(controller.cardPane())
+            add(controller.background())
+            add(controller.background())
+        }
+        row {
+            add(controller.background())
+            add(controller.background())
+            add(controller.background())
+            //add(controller.cardPane())
+            add(controller.background())
+            add(controller.background())
+        }
+        // you can add a lot more rows below as you like.
+        // use this to check placement:
+         isGridLinesVisible = false
+    }
+}
+
+class Styles : Stylesheet() {
+    companion object {
+        val main by cssclass()
+        val neighborhood by cssclass()
     }
 
+    init {
+        main {
+            backgroundColor += c("4E9830")
+        }
+
+        neighborhood {
+            backgroundColor += c("4E9830")
+            prefWidth = 470.px
+            prefHeight = 590.px
+        }
+    }
+}
+
+
+// in the controller package
+class viewController: Controller() {
+
     fun background(): StackPane {
-        return stackpane {
+        return StackPane().apply {
             rectangle {
                 fill = c("4E9830")
-                width = 1000.0
-                height = 1000.0
+                width = 100.0
+                height = 125.0
             }
         }
     }
 
-    fun card(position: Pos): StackPane {
-      return stackpane { rectangle {
-          fill = c("#FFFFFF")
-          width = 100.0
-          height = 160.0
-          alignment=position
-      } }
+    fun cardPane(): StackPane {
+        return StackPane().apply {
+//            rectangle {
+//                fill = c("4E9830")
+//                width = 100.0
+//                height = 100.0
+//            }
+            rectangle {
+                fill = c("#efeffe")
+                width = 100.0
+                height= 125.0
+            }
+        }
     }
+
+    private fun ClosedRange<Int>.random() = Random().nextInt(endInclusive - start) + start
+
 }
